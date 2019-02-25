da1ce6ed9e9552b91c5ba201ddb555388efc2f2e
Shemar Dacosta <dshemar6@gmail.com>
2019-02-25 14:41:36 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create(first_name: "SHEMAR", last_name: "DACOSTA")
+
+Student.create(first_name: "JAMAR", last_name: "DACOSTA")
